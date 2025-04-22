1065d4ecc22dde2e235e6eff01448ee3944c0a75
to236663 <tommyfernandez099@gmail.com>
2025-04-21 23:22:40 -0400

    changes to links

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -7,9 +7,9 @@ function Contact() {
       <p>Feel free to reach out to me through the following links!</p>
       <ul className="contact-links">
         <li>Email - <a href="mailto:tommyfernandez099@gmail.com">tommyfernandez099@gmail.com</a></li>
-        <li>LinkedIn - <a href="https://www.linkedin.com/in/tommy-fernandez-32123421b/">linkedin.com/tommy-fernandez</a></li>
-        <li>Handshake - <a href="https://ucf.joinhandshake.com/profiles/tz6dn2">joinhandshake.com/tommy-fernandez</a></li>
-        <li>GitHub - <a href="https://github.com/to236663">github.com/to236663</a></li>
+        <li>LinkedIn - <a href="https://www.linkedin.com/in/tommy-fernandez-32123421b/" target='_blank' rel='noopener noreferrer'>linkedin.com/tommy-fernandez</a></li>
+        <li>Handshake - <a href="https://ucf.joinhandshake.com/profiles/tz6dn2" target='_blank' rel='noopener noreferrer'>joinhandshake.com/tommy-fernandez</a></li>
+        <li>GitHub - <a href="https://github.com/to236663" target='_blank' rel='noopener noreferrer'>github.com/to236663</a></li>
       </ul>
     </div>
   );
